76d65f065d4a5653f875495420b3513c79b60fff
Pancho Arana <panchoarana2014@gmail.com>
2023-10-24 21:46:40 -0300

    #HU - Update

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -31,81 +31,90 @@ export class ScheduleService {
 
   ) { }
 
-  async createSchedule(createScheduleDto: CreateScheduleDto) {
-    const { days, turnDuration, supplier, dates } = createScheduleDto;
-  
-    // Obtén la fecha de inicio y fin para los turnos
-    const startTime = dateFns.parse(createScheduleDto.initialTurnDateTime, 'HH:mm', new Date());
-    const endTime = dateFns.parse(createScheduleDto.finalTurnDateTime, 'HH:mm', new Date());
-  
-    const supplierFound = await this.supplierRepository.findOne({ where: { user: supplier?.id as any } });
-  
-    const savedSchedule = await this.entityManager.transaction(async transactionalEntityManager => {
-      const schedule = new Schedule();
-      schedule.name = createScheduleDto.name;
-      schedule.hasSign = createScheduleDto.hasSign;
-      schedule.turnDuration = createScheduleDto.turnDuration;
-      schedule.initialTurnDateTime = startTime.toISOString();
-      schedule.finalTurnDateTime = endTime.toISOString();
-      schedule.supplier = supplierFound;
-      const savedSchedule = await transactionalEntityManager.save(Schedule, schedule);
-  
-      for (const selectedDate of dates) {
-        const selectedDayOfWeek = dateFns.format(new Date(selectedDate), 'EEEE', { locale: es }); // Obtén el nombre del día de la semana en español
-        let capitalizedSelectedDayOfWeek = selectedDayOfWeek.charAt(0).toUpperCase() + selectedDayOfWeek.slice(1);
-        if(capitalizedSelectedDayOfWeek === "Miércoles"){
-          capitalizedSelectedDayOfWeek = "Miercoles"
+// ...
+
+async createSchedule(createScheduleDto: CreateScheduleDto) {
+  const { days, turnDuration, supplier, dates } = createScheduleDto;
+
+  // Obtén la fecha de inicio y fin para los turnos
+  const startTime = dateFns.parse(createScheduleDto.initialTurnDateTime, 'HH:mm', new Date());
+  const endTime = dateFns.parse(createScheduleDto.finalTurnDateTime, 'HH:mm', new Date());
+
+  // Verifica si la duración del turno es menor o igual al tiempo disponible
+  const timeDiff = dateFns.differenceInMinutes(endTime, startTime);
+  if (turnDuration > timeDiff) {
+    throw new Error('La duración del turno excede el tiempo disponible.');
+  }
+
+  const supplierFound = await this.supplierRepository.findOne({ where: { user: supplier?.id as any } });
+
+  const savedSchedule = await this.entityManager.transaction(async transactionalEntityManager => {
+    const schedule = new Schedule();
+    schedule.name = createScheduleDto.name;
+    schedule.hasSign = createScheduleDto.hasSign;
+    schedule.turnDuration = createScheduleDto.turnDuration;
+    schedule.initialTurnDateTime = startTime.toISOString();
+    schedule.finalTurnDateTime = endTime.toISOString();
+    schedule.supplier = supplierFound;
+    const savedSchedule = await transactionalEntityManager.save(Schedule, schedule);
+
+    for (const selectedDate of dates) {
+      const selectedDayOfWeek = dateFns.format(new Date(selectedDate), 'EEEE', { locale: es });
+      let capitalizedSelectedDayOfWeek = selectedDayOfWeek.charAt(0).toUpperCase() + selectedDayOfWeek.slice(1);
+      if (capitalizedSelectedDayOfWeek === "Miércoles") {
+        capitalizedSelectedDayOfWeek = "Miercoles";
+      }
+      const modifiedDays = days.map(day => {
+        if (day === 'Sunday') {
+          return 'Domingo';
+        } else if (day === 'Monday') {
+          return 'Lunes';
+        } else if (day === 'Tuesday') {
+          return 'Martes';
+        } else if (day === 'Wednesday') {
+          return 'Miercoles';
+        } else if (day === 'Thursday') {
+          return 'Jueves';
+        } else if (day === 'Friday') {
+          return 'Viernes';
+        } else {
+          return 'Sabado';
         }
-        const modifiedDays = days.map(day => {
-          if (day === 'Sunday') {
-            return 'Domingo';
-          } else if (day === 'Monday') {
-            return 'Lunes';
-          } else if (day === 'Tuesday') {
-            return 'Martes';
-          } else if (day === 'Wednesday') {
-            return 'Miercoles';
-          } else if (day === 'Thursday') {
-            return 'Jueves';
-          } else if (day === 'Friday') {
-            return 'Viernes';
-          } else {
-            return 'Sabado';
-          }
-        });
-        
-        if (modifiedDays.includes(capitalizedSelectedDayOfWeek as any) ) { // Verifica si el día coincide con los días seleccionados
-          const classDayType = await this.entityManager.findOneOrFail(Type, { where: { name: capitalizedSelectedDayOfWeek } });
-  
-          let currentTime = dateFns.setHours(new Date(selectedDate), dateFns.getHours(startTime));
-          currentTime = dateFns.setMinutes(currentTime, dateFns.getMinutes(startTime));
-  
-          while (dateFns.isBefore(currentTime, dateFns.setHours(new Date(selectedDate), dateFns.getHours(endTime)))) {
-            console.log('entra?');
-            
-            const newTurn = new Turn();
-            const newTurnStatus = new TurnStatus();
-            newTurnStatus.statusRegistrationDateTime = this.formatDate(new Date());
-            newTurnStatus.turnStatusType = await this.validateTypeTurnStatus();
-            newTurn.dateFrom = currentTime as any;
-            newTurn.dateTo = dateFns.addMinutes(currentTime, turnDuration) as any;
-            newTurn.classDayType = classDayType;
-            newTurn.schedule = savedSchedule;
-            newTurn.turnStatus = [newTurnStatus];
-  
-            await transactionalEntityManager.save(TurnStatus, newTurnStatus);
-            await transactionalEntityManager.save(Turn, newTurn);
-  
-            currentTime = dateFns.addMinutes(currentTime, turnDuration);
-          }
+      });
+
+      if (modifiedDays.includes(capitalizedSelectedDayOfWeek as any)) {
+        const classDayType = await this.entityManager.findOneOrFail(Type, { where: { name: capitalizedSelectedDayOfWeek } });
+
+        let currentTime = dateFns.setHours(new Date(selectedDate), dateFns.getHours(startTime));
+        currentTime = dateFns.setMinutes(currentTime, dateFns.getMinutes(startTime));
+
+        while (dateFns.isBefore(currentTime, dateFns.setHours(new Date(selectedDate), dateFns.getHours(endTime))) || dateFns.isSameMinute(currentTime, dateFns.setHours(new Date(selectedDate), dateFns.getHours(endTime)))) {
+          const newTurn = new Turn();
+          const newTurnStatus = new TurnStatus();
+          newTurnStatus.statusRegistrationDateTime = this.formatDate(new Date());
+          newTurnStatus.turnStatusType = await this.validateTypeTurnStatus();
+          newTurn.dateFrom = currentTime as any;
+          newTurn.dateTo = dateFns.addMinutes(currentTime, turnDuration) as any;
+          newTurn.classDayType = classDayType;
+          newTurn.schedule = savedSchedule;
+          newTurn.turnStatus = [newTurnStatus];
+
+          await transactionalEntityManager.save(TurnStatus, newTurnStatus);
+          await transactionalEntityManager.save(Turn, newTurn);
+
+          currentTime = dateFns.addMinutes(currentTime, turnDuration);
         }
       }
-  
-      return savedSchedule;
-    });
-  
+    }
+
     return savedSchedule;
-  }
+  });
+
+  return savedSchedule;
+}
+
+// ...
+
 
   async validateTypeTurnStatus() {
     const turnTypeStatus = await this.typeService.findTypeByCodeJust('TurnoDisponible')
@@ -147,14 +156,14 @@ export class ScheduleService {
       .where('Schedule.id = :id', { id: id })
       // .andWhere('Supplier.id = :idSupplier', {idSupplier: idSupplier})
       .getOne()
-      if(schedule){
-        schedule.turn = schedule.turn.map(turn => ({
-          ...turn,
-          dateFrom: moment(turn.dateFrom).format('YYYY-MM-DDTHH:mm:ss.SSS'),
-          dateTo: moment(turn.dateTo).format('YYYY-MM-DDTHH:mm:ss.SSS'),
-          monthDay: moment(turn.dateFrom).format('DD/MM'),
-        }));
-      }
+    if (schedule) {
+      schedule.turn = schedule.turn.map(turn => ({
+        ...turn,
+        dateFrom: moment(turn.dateFrom).format('YYYY-MM-DDTHH:mm:ss.SSS'),
+        dateTo: moment(turn.dateTo).format('YYYY-MM-DDTHH:mm:ss.SSS'),
+        monthDay: moment(turn.dateFrom).format('DD/MM'),
+      }));
+    }
     return schedule;
   }
 
