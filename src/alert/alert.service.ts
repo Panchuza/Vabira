e65a8cc4e36b40b123f36173639f0aa02d8975eb
Pancho Arana <francisco.arana@segurossura.com.ar>
2024-01-17 17:58:07 -0300

    update

--- src/alert/alert.service.ts
+++ src/alert/alert.service.ts
@@ -2,18 +2,38 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Alert } from '../entities/alert.entity';
+let cron = require('node-cron');
 
 @Injectable()
 export class AlertService {
   constructor(
     @InjectRepository(Alert)
     private readonly alertRepository: Repository<Alert>,
-  ) {}
+    ) {}
 
-  async getAlerts(): Promise<Alert[]> {
-    return await this.alertRepository.find({
-      relations: ['turn', 'turn.schedule.supplier', 'turn.client'],
-    });
+
+  async getAlertsForClient(id: string) {
+    const alerts = await this.alertRepository.createQueryBuilder('alert')
+      .leftJoinAndSelect('alert.turn', 'turn')
+      .leftJoinAndSelect('turn.client', 'client')
+      .where('client.id = :clientId', { clientId: id })
+      .getMany();
+    return alerts;
   }
-  
+
+  async getAlertsForSupplier(id: string) {
+    console.log('entra ?');
+    
+    console.log('aca?', id);
+    const alerts = await this.alertRepository.createQueryBuilder('alert')
+    .leftJoinAndSelect('alert.turn', 'turn')
+    .leftJoinAndSelect('turn.client', 'client')
+    .leftJoinAndSelect('turn.schedule', 'schedule')
+    .leftJoinAndSelect('schedule.supplier', 'supplier')
+    .where('supplier.id = :supplierId', { supplierId: id })
+    .getMany();
+      
+    return alerts;
+  }
+
 }
